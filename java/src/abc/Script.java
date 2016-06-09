f28efbda1e4459c24693e27956b77b7785ad8b30
BBK-PiJ-2015-10 <yasserpo@hotmail.com>
2016-06-09 12:50:14 +0100

    This a set up testing.

---
+++ java/src/abc/Script.java
@@ -0,0 +1,14 @@
+package abc;
+
+public class Script {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is a test");
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
