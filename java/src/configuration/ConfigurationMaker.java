84db8a614da047cfc789e7e6c6764f607f490c1e
BBK-PiJ-2015-10 <yasserpo@hotmail.com>
2016-09-15 17:08:38 +0100

    Updates: WIP JavaDocs.

--- java/src/configuration/ConfigurationMaker.java
+++ java/src/configuration/ConfigurationMaker.java
@@ -3,8 +3,24 @@ package configuration;
 import user.UserSpace;
 import java.util.Scanner;
 
+/**
+ * @author YasserAlejandro
+ * Classes implementing ConfigurationMaker will create a Configuration space on a
+ * particular location and start a ConfigurationMapper object.
+ */
 public interface ConfigurationMaker {
 	
+	/**
+	 * This method captures the desired configName from the user, 
+	 * creates a directory on disk based on that name, and triggers
+	 * a ConfigurationMapper object.
+	 * 
+	 * @param userSpace that will host this configuration.
+	 * @param sc a scanner will capture user input (configuration name).
+	 * @param configType the type of configuration to create.
+	 * @return boolean true if the configuration making processes succeeded,
+	 * false if not.
+	 */
 	boolean makeConfiguration(UserSpace userSpace,Scanner sc,String configType);
 	
 	
--- java/src/configuration/ConfigurationMapper.java
+++ java/src/configuration/ConfigurationMapper.java
@@ -2,8 +2,25 @@ package configuration;
 
 import java.util.Scanner;
 
+/**
+ * @author YasserAlejandro
+ * Classes implementing a ConfigurationMapper should:
+ * - Create a map file to host the mapping of general ledger dimensions of
+ * interest to BPA driver sources.
+ * - Extract from a general ledger source the unique value sets of dimensions
+ * of interest and capture from the user the BPA source driver for each unique
+ * set of values. 
+ */
 public interface ConfigurationMapper {
-		
+	
+	/**
+	 * Creates a map file and executes the mapping of general ledger dimensions
+	 * of interests to BPA driver sources.
+	 * 
+	 * @param sc a scanner object to capture data from the user.
+	 * @return a Boolean true if the mapping of files was successful. Return a 
+	 * Boolean false if the operation failed.
+	 */
 	boolean mapFiles(Scanner sc);
 
 }
